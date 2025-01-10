90b242ba7e317841cd3dfbde2983924dbfa6cd16
F <72967176+silvertras@users.noreply.github.com>
2025-01-10 22:52:15 +0700

    Update listNumbers.js

--- api/listNumbers.js
+++ api/listNumbers.js
@@ -1,11 +1,19 @@
-const Number = require('../models/numberModel');
+import dbConnect from "../../utils/dbConnect";
+import NumberModel from "../../models/numberModel";
 
-module.exports = async (req, res) => {
-    try {
-        const numbers = await Number.find();
-        return res.status(200).json({ data: numbers });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Internal server error.' });
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        try {
+            await dbConnect();
+            const numbers = await NumberModel.find().limit(100); // Batasi jumlah data yang diambil
+
+            return res.status(200).json({ success: true, data: numbers });
+
+        } catch (error) {
+            console.error('Error listing numbers:', error);
+            return res.status(500).json({ success: false, message: 'Server Error' });
+        }
+    } else {
+        res.status(405).json({ success: false, message: 'Method Not Allowed' });
     }
-};
+}
